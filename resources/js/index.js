3a117127dac671d12155200664484df7f1b8db5a
mezmeri <madsschmidt33@gmail.com>
2023-06-20 10:43:10 +0200

    style fixes

--- resources/js/index.js
+++ resources/js/index.js
@@ -5,7 +5,7 @@ const starterCardButton = document.getElementById('starter-card-button');
 const establishedCardButton = document.getElementById('established-card-button');
 
 starterCardButton.addEventListener('click', () => {
-    priceCard_starter.style.transition = 'outline 0.1s ease';
+    priceCard_starter.style.transition = 'outline 0.05s ease-in';
     priceCard_starter.style.outline = '3px solid rgb(118, 28, 255)';
     priceCard_starter.style.outlineOffset = '5px';
 
@@ -14,7 +14,7 @@ starterCardButton.addEventListener('click', () => {
 });
 
 establishedCardButton.addEventListener('click', () => {
-    priceCard_established.style.transition = 'outline 0.1s ease';
+    priceCard_established.style.transition = 'outline 0.05s ease-in';
     priceCard_established.style.outline = '3px solid rgb(118, 28, 255)';
     priceCard_established.style.outlineOffset = '5px';
 
